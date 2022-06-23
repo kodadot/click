c5de70cf262ffa490c4d9bb52c1e683d470f1f58
Viki Val <viktorko99@gmail.com>
2022-06-23 12:27:29 +0200

    :zap: helper to decode 721 transfer

--- src/mappings/utils/evm.ts
+++ src/mappings/utils/evm.ts
@@ -1,4 +1,6 @@
 import { EMPTY_ADDRESS } from './constants'
+import * as erc721 from '../../abi/erc721'
+import { Context } from './types'
 
 
 export const isMint = (addrOne: string, addrTwo: string) => {
@@ -12,4 +14,8 @@ export const isBurn = (addrOne: string, addrTwo: string) => {
 
 export const isTransfer = (addrOne: string, addrTwo: string) => {
   return !(isMint(addrOne, addrTwo) || isBurn(addrOne, addrTwo))
+}
+
+export function decode721Transfer(event: Context): erc721.TransferAddressAddressUint256Event {
+  return erc721.events["Transfer(address,address,uint256)"].decode(event)
 }
