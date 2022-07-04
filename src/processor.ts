e30ed4a05a30b0bf80a6def80569b8299a73ce63
Viki Val <viktorko99@gmail.com>
2022-07-04 13:05:40 +0200

    :construction: soon ready for 1155

--- src/processor.ts
+++ src/processor.ts
@@ -4,7 +4,7 @@ import {
 } from "@subsquid/substrate-evm-processor"
 import { CHAIN_NODE } from "./contract"
 import * as mappings from './mappings'
-import { transferFilter } from './mappings/utils/evm'
+import { transferFilter, singleTransferFilter } from './mappings/utils/evm'
 import { Contracts } from './processable'
 
 const processor = new SubstrateEvmProcessor("moonriver-substrate");
@@ -24,10 +24,16 @@ processor.setTypesBundle("moonbeam");
 
 processor.addPreHook({ range: { from: 0, to: 0 } }, mappings.forceCreateContract);
 
-processor.addEvmLogHandler(Contracts.Moonsama, transferFilter, mappings.mainFrame);
-processor.addEvmLogHandler(Contracts.Pondsama, transferFilter, mappings.mainFrame);
-processor.addEvmLogHandler(Contracts.Plot, transferFilter, mappings.mainFrame);
-// processor.addEvmLogHandler(Contracts.Blvck, transferFilter, mappings.mainFrame);
+// processor.addEvmLogHandler(Contracts.Moonsama, transferFilter, mappings.mainFrame);
+// processor.addEvmLogHandler(Contracts.Pondsama, transferFilter, mappings.mainFrame);
+// processor.addEvmLogHandler(Contracts.Plot, transferFilter, mappings.mainFrame);
+// processor.addEvmLogHandler(Contracts.Blvck, transferFilter, mappings.mainFrame); // TODO: handle separately
+
+processor.addEvmLogHandler(Contracts.Moonx, singleTransferFilter, mappings.singleMainFrame);
+// processor.addEvmLogHandler(Contracts.Factory, singleTransferFilter, mappings.singleMainFrame);
+// processor.addEvmLogHandler(Contracts.Art, singleTransferFilter, mappings.singleMainFrame);
+// processor.addEvmLogHandler(Contracts.Box, singleTransferFilter, mappings.singleMainFrame);
+// processor.addEvmLogHandler(Contracts.Embassy, singleTransferFilter, mappings.singleMainFrame);
 
 // processor.addEvmLogHandler(
 //   Contracts.Moonx,
