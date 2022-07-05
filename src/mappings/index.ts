08c58e4941caec42c4e506b66824dd4240feb1e9
Viki Val <viktorko99@gmail.com>
2022-07-05 12:54:10 +0200

    :construction: 1155 :pray:

--- src/mappings/index.ts
+++ src/mappings/index.ts
@@ -7,14 +7,14 @@ import {
   NFTEntity as NE
 } from '../model'
 import { ContractsMap } from '../processable'
-import { plsBe, real, remintable } from './utils/consolidator'
+import { created, plsBe, real, remintable } from './utils/consolidator'
 import { EMPTY_ADDRESS } from './utils/constants'
 import { create, get, getOrCreate } from './utils/entity'
 import { decode1155SingleTransfer, decode721Transfer, RealTransferEvent, whatIsThisTransfer } from './utils/evm'
 import { createFungibleTokenId, createTokenId, unwrap } from './utils/extract'
 import {
   getBurnTokenEvent, getCreateCollectionEvent,
-  getCreateTokenEvent, getSingleCreateTokenEvent, getTokenUriChangeEvent, getTransferTokenEvent
+  getCreateTokenEvent, getSingleBurnTokenEvent, getSingleCreateTokenEvent, getSingleTransferTokenEvent, getTokenUriChangeEvent, getTransferTokenEvent
 } from './utils/getters'
 import { isEmpty } from './utils/helper'
 import logger, { logError, metaLog } from './utils/logger'
@@ -23,11 +23,12 @@ import { findAll1155Tokens } from './utils/query'
 import { serializer } from './utils/serializer'
 import {
   attributeFrom,
-  BaseCall, Context, ensure,
+  BaseCall, Context, CreateTokenEvent, ensure,
   eventFrom,
   eventId,
   Interaction, Optional,
   TokenMetadata,
+  TransferSingleTokenEvent,
   WithCount
 } from './utils/types'
 
@@ -128,6 +129,93 @@ export async function handleTokenCreate(context: Context): Promise<void> {
   await createEvent(final, Interaction.MINTNFT, event, '', context.store)
 }
 
+export async function handleSingleTokenCreate(context: Context, fromTransfer: boolean = false): Promise<void> {
+  if (!fromTransfer) {
+    logger.pending(`[NFT++]: ${context.substrate.block.height}`)
+  }
+  const event = unwrap(context, getSingleCreateTokenEvent)
+  metaLog('Fungible', event)
+  const id = createFungibleTokenId(event.collectionId, event.sn, event.caller)
+  const collection = ensure<CE>(
+    await get<CE>(context.store, CE, event.collectionId)
+  )
+  const final = await getOrCreate<NE>(context.store, NE, id, {})
+  plsBe(real, collection)
+
+  if (created(final)) {
+    final.id = id
+    final.hash = md5(id)
+    final.issuer = event.caller
+    final.currentOwner = event.caller
+    final.blockNumber = BigInt(event.blockNumber)
+    final.collection = collection
+    final.sn = event.sn
+    final.price = BigInt(0);
+    final.metadata = await event.metadata
+    final.burned = false
+    final.createdAt = event.timestamp
+    final.updatedAt = event.timestamp
+    final.count = event.count
+  
+    logger.debug(`metadata: ${final.metadata}`)
+  
+    if (final.metadata) {
+      const metadata = await handleMetadata(final.metadata, context.store)
+      final.meta = metadata
+      final.name = metadata?.name
+    }
+    logger.success(`[MINT] ${final.id}`)
+  } else {
+    final.count += event.count
+    final.burned = false
+    logger.success(`[TRANSFER] Add ${event.count} tokens to ${event.caller}`)
+  }
+
+  
+  await context.store.save(final)
+  await createEvent(final, Interaction.MINTNFT, event, '', context.store)
+}
+
+export async function handleSingleTokenTransfer(context: Context): Promise<void> {
+  logger.pending(`[SEND]: ${context.substrate.block.height}`)
+  const event = unwrap(context, getSingleTransferTokenEvent)
+  logger.debug(`send: ${JSON.stringify(event, serializer, 2)}`)
+  await handleSingleTokenBurn(context)
+  await handleSingleTokenCreate(context)
+  logger.success(
+    `[SEND] from ${event.caller} to ${event.to}`
+  )
+  
+  
+}
+
+export async function handleSingleTokenBurn(context: Context, fromTransfer: boolean = false): Promise<void> {
+  if (!fromTransfer) {
+    logger.pending(`[BURN]: ${context.substrate.block.height}`)
+  }
+  const event = unwrap(context, getSingleBurnTokenEvent)
+  logger.debug(`burn: ${JSON.stringify(event, serializer, 2)}`)
+  const id = createTokenId(event.collectionId, event.sn)
+  const entity = ensure<NE>(await get(context.store, NE, id))
+  plsBe(real, entity)
+
+  entity.count -= event.count
+
+  if (entity.count === 0) {
+    entity.burned = true
+  }
+
+  if (!fromTransfer) {
+    logger.success(`[BURN] ${id} by ${event.caller}`)
+  } else {
+    logger.success(`[TRANSFER] Substract ${event.count} tokens from ${event.caller}`)
+  }
+  
+  await context.store.save(entity)
+  const meta = String(event.count)
+  await createEvent(entity, Interaction.CONSUME, event, meta, context.store)
+}
+
 export async function handleTokenTransfer(context: Context): Promise<void> {
   logger.pending(`[SEND]: ${context.substrate.block.height}`)
   const event = unwrap(context, getTransferTokenEvent)
@@ -215,7 +303,7 @@ export function singleMainFrame(ctx: Context): Promise<void> {
   return technoBunker(ctx, transfer, CollectionType.ERC1155)
 }
 
-async function technoBunker(ctx: Context, transfer: RealTransferEvent) {
+async function technoBunker(ctx: Context, transfer: RealTransferEvent, type = CollectionType.ERC721) {
   switch (whatIsThisTransfer(transfer)) {
     case Interaction.MINTNFT:
       metaLog(Interaction.MINTNFT, transfer)
