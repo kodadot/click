735e74f5c39767895b3987cc99561f7c0fc1d37b
Viki Val <viktorko99@gmail.com>
2022-08-08 12:09:50 +0200

    :construction: debugging

--- src/processor.ts
+++ src/processor.ts
@@ -1,19 +1,27 @@
 import { lookupArchive } from "@subsquid/archive-registry"
 import {
-  SubstrateEvmProcessor
-} from "@subsquid/substrate-evm-processor"
+  EvmLogHandlerContext,
+  SubstrateProcessor,
+} from "@subsquid/substrate-processor";
+import { FullTypeormDatabase as Database } from '@subsquid/typeorm-store'
 import { CHAIN_NODE } from "./contract"
 import * as mappings from './mappings'
 import { multiTransferFilter, singleTransferFilter, transferFilter } from './mappings/utils/evm'
 import { Contracts } from './processable'
+import * as erc721 from "./abi/erc721";
+import { Context } from './mappings/utils/types'
+import { toBaseEvent, unwrap } from './mappings/utils/extract'
+import logger from './mappings/utils/logger'
+import { serializer } from './mappings/utils/serializer'
 
-const processor = new SubstrateEvmProcessor("moonriver-substrate");
+const database = new Database();
+const processor = new SubstrateProcessor(database)
 
 processor.setBatchSize(500);
 
 processor.setDataSource({
   chain: CHAIN_NODE,
-  archive: lookupArchive("moonriver")[0].url,
+  archive: lookupArchive("moonriver", { release: "FireSquid" }),
 });
 
 processor.setTypesBundle("moonbeam");
@@ -22,38 +30,39 @@ processor.setTypesBundle("moonbeam");
 //   await ctx.store.save(createContractEntity());
 // });
 
-processor.addPreHook({ range: { from: 0, to: 0 } }, mappings.forceCreateContract);
+// processor.addPreHook({ range: { from: 0, to: 0 } }, mappings.forceCreateContract);
 
-processor.addEvmLogHandler(Contracts.Moonsama, transferFilter, mappings.mainFrame);
-processor.addEvmLogHandler(Contracts.Pondsama, transferFilter, mappings.mainFrame);
-processor.addEvmLogHandler(Contracts.Plot, transferFilter, mappings.mainFrame);
-// processor.addEvmLogHandler(Contracts.Blvck, transferFilter, mappings.mainFrame); // TODO: handle separately
+// processor.addEvmLogHandler(Contracts.Moonsama, transferFilter, mappings.mainFrame);
+// processor.addEvmLogHandler(Contracts.Pondsama, transferFilter, mappings.mainFrame);
+// processor.addEvmLogHandler(Contracts.Plot, transferFilter, mappings.mainFrame);
+// // processor.addEvmLogHandler(Contracts.Blvck, transferFilter, mappings.mainFrame); // TODO: handle separately
 
-processor.addEvmLogHandler(Contracts.Moonx, singleTransferFilter, mappings.singleMainFrame);
-processor.addEvmLogHandler(Contracts.Factory, singleTransferFilter, mappings.singleMainFrame);
-processor.addEvmLogHandler(Contracts.Art, singleTransferFilter, mappings.singleMainFrame);
-processor.addEvmLogHandler(Contracts.Box, singleTransferFilter, mappings.singleMainFrame);
-processor.addEvmLogHandler(Contracts.Embassy, singleTransferFilter, mappings.singleMainFrame);
+// processor.addEvmLogHandler(Contracts.Moonx, singleTransferFilter, mappings.singleMainFrame);
+// processor.addEvmLogHandler(Contracts.Factory, singleTransferFilter, mappings.singleMainFrame);
+// processor.addEvmLogHandler(Contracts.Art, singleTransferFilter, mappings.singleMainFrame);
+// processor.addEvmLogHandler(Contracts.Box, singleTransferFilter, mappings.singleMainFrame);
+// processor.addEvmLogHandler(Contracts.Embassy, singleTransferFilter, mappings.singleMainFrame);
 
-processor.addEvmLogHandler(Contracts.Moonx, multiTransferFilter, mappings.mutliMainFrame);
-processor.addEvmLogHandler(Contracts.Factory, multiTransferFilter, mappings.mutliMainFrame);
-processor.addEvmLogHandler(Contracts.Art, multiTransferFilter, mappings.mutliMainFrame);
-processor.addEvmLogHandler(Contracts.Box, multiTransferFilter, mappings.mutliMainFrame);
-processor.addEvmLogHandler(Contracts.Embassy, multiTransferFilter, mappings.mutliMainFrame);
+// processor.addEvmLogHandler(Contracts.Moonx, multiTransferFilter, mappings.mutliMainFrame);
+// processor.addEvmLogHandler(Contracts.Factory, multiTransferFilter, mappings.mutliMainFrame);
+// processor.addEvmLogHandler(Contracts.Art, multiTransferFilter, mappings.mutliMainFrame);
+// processor.addEvmLogHandler(Contracts.Box, multiTransferFilter, mappings.mutliMainFrame);
+// processor.addEvmLogHandler(Contracts.Embassy, multiTransferFilter, mappings.mutliMainFrame);
 
-// processor.addEvmLogHandler(
-//   Contracts.Moonx,
-//   multiTransferFilter,
-//   contractLogsHandler
-// );
+processor.addEvmLogHandler(
+  Contracts.Moonsama,
+  {...transferFilter, range: { from: 569006, to: 569006 }},
+  contractLogsHandler
+);
 
-// export async function contractLogsHandler(
-//   ctx: EvmLogHandlerContext
-// ): Promise<void> {
-//   const data = unwrap(ctx, getMultiCreateTokenEvent)
+export async function contractLogsHandler(
+  ctx: Context
+): Promise<void> {
+  // const data = toBaseEvent(ctx)
+  const data = { block: ctx.block, event: ctx.event }
 
-//   logger.debug(`Transfer: ${JSON.stringify(data, serializer, 2)}`)
-//   logger.debug(`contractAddress: ${ctx.contractAddress}`)
-// }
+  logger.debug(`Transfer: ${JSON.stringify(data, serializer, 2)}`)
+  // logger.debug(`contractAddress: ${ctx.contractAddress}`)
+}
 
 processor.run();
