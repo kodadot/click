db2881b68d5b31dc48e8e8846af8f74755352c0b
Viki Val <viktorko99@gmail.com>
2022-10-06 12:47:40 +0200

    :zap: added moonsama contracts

--- src/processable.ts
+++ src/processable.ts
@@ -20,6 +20,14 @@ export enum Contracts {
   BlvckSnakesForrest  = '0x09a7f6e904bd6293ed382e905895efd0983f325f',
   CryptoButchers = '0x0dad866dc0c13fb8e4a91d1b5e83cf3a61d4cee2',
   MoonShroomiz = '0x2f26efdb7233a014715ce6e895aa67d846d93f1e',
+  Moonsama = '0xb654611f84a8dc429ba3cb4fda9fad236c505a1a', // ERC-721
+  Pondsama = '0xe4edcaaea73684b310fc206405ee80abcec73ee0', // ERC-721
+  Moonx = '0x1974eeaf317ecf792ff307f25a3521c35eecde86', // ERC-1155
+  Factory = '0x1b30a3b5744e733d8d2f19f0812e3f79152a8777', // ERC-1155
+  Art = '0xdea45e7c6944cb86a268661349e9c013836c79a2', // ERC-1155
+  Plot = '0xa17a550871e5f5f692a69a3abe26e8dbd5991b75', // ERC-721
+  Box = '0xd335417999ff2b9b59737244e554370264b3f877', // ERC-1155
+  Embassy = '0x0a54845ac3743c96e582e03f26c3636ea9c00c8a', // ERC-1155
 }
 
 export const ContractsMap: Record<Contracts, any>  = {
@@ -37,6 +45,14 @@ export const ContractsMap: Record<Contracts, any>  = {
   [Contracts.BlvckSnakesForrest]: toMap('BlvckMarketDvrkForest', 'BMADVRKFOREST', 717, '0x56b0b239508f63C5a38558c6987cca0195d62039', 'QmYSThWjHh3swx2qXxGM6MMdv35hvCmaptNZFDXZisUHzi/1.json'),
   [Contracts.CryptoButchers]: toMap('CryptoButchers', 'BUTCHER', 7776, '0xaE9ccb445f793ED458e73505644ef2FADfb17Ac8', 'QmVBDKihHMMnbMysmazLzZck5ySE2wJjbHChFVC7qzAw5W/1.json'),
   [Contracts.MoonShroomiz]: toMap('MoonShroomiz', 'MSHZ', 500, '0xbCe5F532Fdba96F131682E97ce4aFEa8b778480C', 'QmXvfjHAiaJHTu5692YofHZunvRJdZiPpcepw8pu4B3wuf/1.json'),
+  [Contracts.Moonsama]: toMap('Moonsama', 'MSAMA', 1000, '0x05b9b543328d4c797e1eec747efc65d97de542f2', 'QmPhFz5mKCtndGLLZBwGockGAWz7o7nef4Kgf37gYsTid5'),
+  [Contracts.Pondsama]: toMap('Pondsama', 'PONDSAMA', 5715, '0x05b9b543328d4c797e1eec747efc65d97de542f2', 'QmdCKgexLpBjST3FdWLbPZLH2FWRtu2NXE9dk5ZirdDRGb'),
+  [Contracts.Moonx]: toMap('MoonX', 'MX', 67, '0x05b9b543328d4c797e1eec747efc65d97de542f2', 'QmWox8YqUaYVxSSB7GRhPAuczv6auL8QrkDrkMTcqGEGKA', CollectionType.ERC1155),
+  [Contracts.Factory]: toMap('Moonsama Multiverse Asset Factory', 'MMAF', 11, '0x05b9b543328d4c797e1eec747efc65d97de542f2', 'Qmc97e79xzzrdNuU3RumGXcK5FYh8PWTb6GyxxHMyRvUYm', CollectionType.ERC1155),
+  [Contracts.Art]: toMap('Moonsama Multiverse Art', 'MMA', 28, '0x05b9b543328d4c797e1eec747efc65d97de542f2', 'QmUCMVkJa849UQYN728hyiYm3ZJVPk8yGyiJQ6wwHfLcgz', CollectionType.ERC1155),
+  [Contracts.Plot]: toMap('Moonsama Minecraft Plots Season 1', 'MMPLOTS1', 338, '0x05b9b543328d4c797e1eec747efc65d97de542f2', 'QmR8K7eLZnhFqC5qUStMyFAgawSJcpRfSvMDMffQVQFn38'),
+  [Contracts.Box]: toMap('SamaBox', 'SAMABOX', 2, '0x05b9b543328d4c797e1eec747efc65d97de542f2', 'QmPisJNXRvd1h8BBiBs1PHu8666HTDWVJWkAf2AS7c4zkM', CollectionType.ERC1155),
+  [Contracts.Embassy]: toMap('MoonsamaEmbassy', 'MEMBASSY', 4, '0x05b9b543328d4c797e1eec747efc65d97de542f2', 'QmWzBDhV5nuPrud7XZo2vBLBWtTStwc6N8xSwCzuMdewd8', CollectionType.ERC1155),
 }
 
 function toMap(name: string, symbol: string, max: number, issuer: string, meta: string, type: CollectionType = CollectionType.ERC721): Partial<CollectionEntity> {
