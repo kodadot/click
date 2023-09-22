53bccb3f0aac1f9da368760a8aaae2429f641cc6
Viki Val <viktorko99@gmail.com>
2023-09-22 13:57:46 +0100

    :heavy_minus_sign: signale

--- src/mappings/utils/logger.ts
+++ src/mappings/utils/logger.ts
@@ -1,8 +1,20 @@
-import singale from 'signale'
+import { serializer } from '@kodadot1/metasquid'
+import { logger } from '@kodadot1/metasquid/logger'
 import { RealTransferEvent } from './evm'
-import { serializer } from './serializer'
 
 type ErrorCallback = (error: Error) => void
+
+type Action = any
+
+export const metaLog = (
+  interaction: string,
+  everythingElse: any
+) =>
+  logger.debug(
+    `[[${interaction}]]: ${JSON.stringify(everythingElse, serializer, 2)}`
+  )
+
+export const transferDebug = (interaction: string, transfer: RealTransferEvent) => logger.debug(`[[${interaction}]]: [${transfer.from}, ${transfer.to}, ${(transfer as any).tokenId || (transfer as any).id}, ${(transfer as any).value || 1}]`)
 
 export const logError = (e: Error | unknown, cb: ErrorCallback) => {
   if (e instanceof Error) {
@@ -10,14 +22,24 @@ export const logError = (e: Error | unknown, cb: ErrorCallback) => {
   }
 }
 
-export const metaLog = (
-  interaction: string,
-  everythingElse: any
-) =>
-  singale.debug(
-    `[[${interaction}]]: ${JSON.stringify(everythingElse, serializer, 2)}`
-  )
+export const success = (action: Action, message: string) => {
+  logger.info(`💚 [${action}] ${message}`)
+}
 
-export const transferDebug = (interaction: string, transfer: RealTransferEvent) => singale.debug(`[[${interaction}]]: [${transfer.from}, ${transfer.to}, ${(transfer as any).tokenId || (transfer as any).id}, ${(transfer as any).value || 1}]`)
+export const error = (e: Error | unknown, action: Action, message: string) => {
+  logError(e, (e) => logger.error(`💔 [${action}] ${e.message} ${message}`))
+}
 
-export default singale
+export const pending = (action: Action, message: string) => {
+  logger.info(`⏳ [${action}] ${message}`)
+}
+
+export const debug = (action: Action, message: Record<any, any>, serialize?: boolean) => {
+  logger.debug(`[${action}] ${JSON.stringify(message, serialize ? serializer : undefined, 2)}`)
+}
+
+export const warn = (action: Action, message: string) => {
+  logger.warn(`⚠️ [${action}] ${message}`)
+}
+
+export { logger as default } from '@kodadot1/metasquid/logger'
